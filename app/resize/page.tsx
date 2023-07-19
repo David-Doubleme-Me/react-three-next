5d9b1d487cbc2341abfc9b5910c982300e3bf9ac
David Kim <david.kim@doubleme.me>
2023-07-20 06:53:26 +0900

    feat: zoomin

---
+++ app/resize/page.tsx
@@ -0,0 +1,10 @@
+import ResizeScene from '@/components/scenes/ResizeScene'
+import React from 'react'
+
+export default function ResizePage() {
+  return (
+    <div className='relative w-full h-full'>
+      <ResizeScene />
+    </div>
+  )
+}
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,15 +2,21 @@
 
 import IconCircleButton from './buttons/IconCircleButton'
 import { AiOutlineMenu } from 'react-icons/ai'
+import MainLogo from './logo/MainLogo'
+import Link from 'next/link'
 
 export default function Navbar() {
   const handleTabMenu = () => {
     console.log('탭 메뉴 클릭!')
   }
+
   return (
     <section>
-      <div className='text-white'>
+      <div className='flex items-center text-white gap-x-4'>
         <IconCircleButton Icon={AiOutlineMenu} onClick={handleTabMenu} />
+        <Link href={`/`}>
+          <MainLogo />
+        </Link>
       </div>
     </section>
   )
