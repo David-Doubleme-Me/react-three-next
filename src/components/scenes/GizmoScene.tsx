e28674e5963aec77301ca97f82a20071ea508807
David Kim <david.kim@doubleme.me>
2023-08-02 14:26:49 +0900

    feat: gizmos

--- src/components/scenes/GizmoScene.tsx
+++ src/components/scenes/GizmoScene.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { OrbitControls, useGLTF, Detailed, Stage, GizmoHelper, GizmoViewport } from '@react-three/drei'
-import { Canvas, useThree } from '@react-three/fiber'
-import { Suspense, useState } from 'react'
-import { Color, Event } from 'three'
+import { OrbitControls, useGLTF, Stage, Grid } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import { Color } from 'three'
 import { GLTF } from 'three-stdlib'
 import ProgressLoading from '../loading/ProgressLoading'
-import CommonViewport from '../helper/gizmo/CommonViewport'
-import CommonViewcube from '../helper/gizmo/CommonViewcube'
+import { useControls } from 'leva'
 
 type ModelProps = {
   url: string
@@ -16,19 +15,37 @@ type ModelProps = {
 const Model = ({ url }: ModelProps) => {
   const { scene } = useGLTF(url) as GLTF
   scene.updateMatrixWorld()
-  return <primitive object={scene} />
+  return (
+    <group>
+      <primitive object={scene} />
+    </group>
+  )
 }
 
 export default function GizmoScene() {
+  const { gridSize, ...girdConfig } = useControls({
+    gridSize: [10.5, 10.5],
+    cellSize: { value: 0.6, min: 0, max: 10, step: 0.1 },
+    cellThickness: { value: 1, min: 0, max: 5, step: 0.1 },
+    cellColor: '#6f6f6f',
+    sectionSize: { value: 3.3, min: 0, max: 10, step: 0.1 },
+    sectionThickness: { value: 1.5, min: 0, max: 5, step: 0.1 },
+    sectionColor: '#9d4b4b',
+    fadeDistance: { value: 25, min: 0, max: 100, step: 1 },
+    fadeStrength: { value: 1, min: 0, max: 1, step: 0.1 },
+    followCamera: false,
+    infiniteGrid: true,
+  })
+
   return (
     <Canvas>
-      <color attach='background' args={[196, 196, 196]} />
+      <color attach='background' args={[50, 50, 50]} />
       <ambientLight />
 
       <Suspense fallback={<ProgressLoading />}>
-        <Stage intensity={0.5} shadows='contact' environment='city'>
-          <Model url={'/model/robot/dji.glb'} />
-        </Stage>
+        {/* <Stage intensity={0.5} shadows='contact' environment='city'> */}
+        <Model url={'/model/robot/dji.glb'} />
+        {/* </Stage> */}
       </Suspense>
 
       <axesHelper
@@ -37,10 +54,9 @@ export default function GizmoScene() {
         onUpdate={(self) => self.setColors(new Color('red'), new Color('green'), new Color('blue'))}
       />
 
-      <CommonViewport />
-      {/* <CommonViewcube /> */}
-
       <OrbitControls makeDefault />
+
+      <Grid args={gridSize} {...girdConfig} />
     </Canvas>
   )
 }
